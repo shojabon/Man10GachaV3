23617bdff356343b0493461512b8c445524bf56d
SHO <shojabon@gmail.com>
2019-03-10 16:13:08 +0900

    Initial commit

---
+++ src/main/java/com/shojabon/man10gachav3/Man10GachaV3.java
@@ -0,0 +1,17 @@
+package com.shojabon.man10gachav3;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Man10GachaV3 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
